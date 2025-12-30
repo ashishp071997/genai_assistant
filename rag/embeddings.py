7504bc89ec3e755ebc751215c9e1912f91e293a5
Ashish Prajapati <ashishprajapati@Ashishs-MacBook-Pro.local>
2025-12-30 19:30:08 +0530

    fixed model load

--- rag/embeddings.py
+++ rag/embeddings.py
@@ -2,9 +2,10 @@
 from sentence_transformers import SentenceTransformer
 import faiss, pickle, os
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+
 
 def build_index():
+    model = SentenceTransformer("all-MiniLM-L6-v2")
     # read policies.txt
     os.makedirs("rag", exist_ok=True)
     with open("genai_assistant/data/policies.txt", "r") as f:
